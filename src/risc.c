d64852c47898f398a5da123ae04c41c1d58c05d1
ajarred <adjarredtan@gmail.com>
2023-09-09 22:32:51 +0800

    Add readline history

--- src/risc.c
+++ src/risc.c
@@ -1,6 +1,26 @@
 #include "../include/decoder.h"
 #include "../include/encoder.h"
 
+#ifdef _WIN32
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
 int main() {
     printf("RISC-V Instruction Codec v.0.0.1\n"
     "Input options:\n"
@@ -9,11 +29,9 @@ int main() {
     "Assembly Instruction (e.g., add x1, x2, x3)\n"
     "Press Ctrl-C to terminate\n");
     while (1) {
-        char input[255];
-        printf("risc>");
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            break; 
-        }
+        char* input = readline("risc> ");
+        add_history(input);
+
         if (strncmp(input, "0x", 2) == 0) {
             unsigned int n;
             if (convertStrToUint(input, &n)) {
@@ -31,6 +49,7 @@ int main() {
         } else {
             fprintf(stderr, "Invalid input.\n");
         }
+        free(input);
     }
     return 0;
 }
